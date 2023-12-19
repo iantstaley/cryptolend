1544f985666859f360b611cf08b92faa6250adb5
iantstaley <ian.t.staley@gmail.com>
2023-12-19 17:10:54 +0500

    added files

---
+++ Readme.md
@@ -0,0 +1,70 @@
+# Cryptolend
+
+[![Build Status](https://travis-ci.org/upscaletech/finocial.eth.svg?branch=master)](https://travis-ci.org/upscaletech/finocial)
+
+Cryptolend Protocol has been created to provide a secure, flexible, open-source foundation for a decentralized loan marketplace on the Ethereum blockchain (as of now). We provide the pieces necessary to create a decentralized lending exchange, including the requisite lending assets, clearing, and collateral pool infrastructure, enabling third parties to build applications for lending.
+
+Join our telegram community to interact with members of our dev staff and other contributors.
+
+## Future
+
+We aim to build
+
+`decentralized credit rating`
+`collecting mechanism - external - internal`
+`payment channels`
+
+## Dependencies
+
+This project uses Node.js version 8.10.0 - 8.11.3.
+
+If you are running multiple versions of Node.js, consider using Node Version Manager (nvm). nvm is an easy way to configure and manage different Node.js versions to work with your projects.
+
+## Getting Started?
+
+A Makefile is provided for easy setup of the environment.
+
+Some prerequisites are required in order to utilize the Makefile.
+
+`git clone https://github.com/Crypto-lend/Cryptolend.git # clone this repository`
+
+From here you will be able to use make commands assuming npm is already installed.
+
+Assuming you have npm already, Install truffle
+
+`make install truffle`
+
+`make install_deps`
+
+## Running ETH Bridge
+
+`npx ethereum-bridge -a 9 -H 127.0.0.1 -p 9545 --dev`
+
+## Tests
+
+Start Truffle and its development blockchain with
+
+`make start_console`
+
+Run the example migrations as well as the accompanying tests inside the truffle console
+
+```
+truffle(develop)> migrate --reset
+truffle(develop)> test
+
+```
+
+### Running tests with coverage enabled
+
+The most convenient way to run tests with coverage enabled is to run them with the help of Docker orchestration. This ensures, that the coverage results will match the ones on Travis CI.
+
+We are working on it.
+
+
+## Contribution
+
+Want to hack on Cryptolend? Awesome!
+
+Cryptolend is an Open Source project and we welcome contributions of all sorts. There are many ways to help, from reporting issues, contributing code, and helping us improve our community.
+
+Ready to jump in? Check docs.Cryptolend.com/#contributing.
---
+++ contracts/libs/DateTime/api.sol
@@ -0,0 +1,44 @@
+/**
+* MIT License
+*
+* Copyright (c) 2019 Finocial
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+pragma solidity ^0.5.0;
+
+
+contract DateTimeAPI {
+        /*
+         *  Abstract contract for interfacing with the DateTime contract.
+         *
+         */
+        // function isLeapYear(uint16 year) constant returns (bool);
+        // function getYear(uint timestamp) constant returns (uint16);
+        // function getMonth(uint timestamp) constant returns (uint8);
+        // function getDay(uint timestamp) constant returns (uint8);
+        // function getHour(uint timestamp) constant returns (uint8);
+        // function getMinute(uint timestamp) constant returns (uint8);
+        // function getSecond(uint timestamp) constant returns (uint8);
+        // function getWeekday(uint timestamp) constant returns (uint8);
+        // function toTimestamp(uint16 year, uint8 month, uint8 day) constant returns (uint timestamp);
+        // function toTimestamp(uint16 year, uint8 month, uint8 day, uint8 hour) constant returns (uint timestamp);
+        // function toTimestamp(uint16 year, uint8 month, uint8 day, uint8 hour, uint8 minute) constant returns (uint timestamp);
+        // function toTimestamp(uint16 year, uint8 month, uint8 day, uint8 hour, uint8 minute, uint8 second) constant returns (uint timestamp);
+}
